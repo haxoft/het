9f52b6ec1f2512119e4b31686bd76be409d4270e
Frank Siegel <fsie@tu-chemnitz.de>
2017-01-20 14:55:53 +0100

    added powershell script for setting up backend

---
+++ startup.ps1
@@ -0,0 +1,13 @@
+start-process powershell.exe -argument '-nologo -noprofile -executionpolicy bypass -command cd $PSScriptRoot; ngrok http 8000 '
+Start-Sleep -m 5000
+$wc = New-Object system.Net.WebClient
+$result = $wc.downloadString('http://localhost:4040/status')
+$result -match 'https://[a-f0-9]+\.ngrok\.io' > $null
+$ngrokUrl = $Matches[0]
+if(!$ngrokUrl) { throw [System.InvalidOperationException] "Could not retrieve ngrok URL" }
+Write-Output $ngrokUrl
+(Get-Content ./hetaddon/static/addon/atlassian-connect.json) | ForEach-Object { $_ -replace '(?<="baseUrl": ").*?(?=")', $ngrokUrl } | Set-Content ./hetaddon/static/addon/atlassian-connect.json
+$ngrokUrlWithoutProtocol = $ngrokUrl -replace 'https://', ''
+(Get-Content ./HET/settings.py) | ForEach-Object { $_ -replace '(?<=ALLOWED_HOSTS = \[").*?(?="\])', $ngrokUrlWithoutProtocol } | Set-Content ./HET/settings.py
+start-process powershell.exe -argument '-nologo -noprofile -executionpolicy bypass -command cd $PSScriptRoot; ./manage.py runserver 8000'
+Read-Host -Prompt "Press Enter to exit"
