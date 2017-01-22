3da9b00357c86f8491724c67c6c843321f87c5c6
Luis Moreno <luis.moreno.de@gmail.com>
2017-01-22 21:56:33 +0100

    add first migration and model

--- HET/settings.py
+++ HET/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')=27kct7y$8j&&1gr@9+o-*(dyuyysvd=9z_g-&kzcr@bkzthg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["2549873e.ngrok.io"]
+ALLOWED_HOSTS = ["7c7a967d.ngrok.io"]
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'HET.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hetdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- hetaddon/models.py
+++ hetaddon/models.py
@@ -1,2 +1,69 @@
 from django.db import models
 
+
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+
+
+class ExternalPlatform(models.Model):
+    platform_id = models.IntegerField()  # is nullable?
+    platform_name = models.CharField(max_length=128)
+    user_ext_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Folder(models.Model):
+    name = models.CharField(max_length=128)
+    parent_folder = models.ForeignKey("self", on_delete=models.CASCADE)
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=128)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=128)
+    created = models.DateTimeField()
+    members = models.ManyToManyField(User, through='Membership')
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    #  some other fields might be added, e.g. role, date, etc.
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=128)
+    type = models.CharField(max_length=128)
+    size = models.FloatField
+    status = models.CharField(max_length=128)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    category = models.ForeignKey('DocumentCategory', on_delete=models.CASCADE)
+
+
+class Requirement(models.Model):
+    name = models.CharField(max_length=128)
+    value = models.CharField(max_length=300)  # Is this enough?
+    disabled = models.BooleanField
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+
+
+class DocumentCategory(models.Model):
+    name = models.CharField(max_length=128)
+
+
+
+
+
+
+
+
+
+
+
+
