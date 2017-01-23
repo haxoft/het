417ed309cc29cde828163392e88c0ef04c1f738a
Frank Siegel <fsie@tu-chemnitz.de>
2017-01-23 17:18:09 +0100

    added some API methods

--- hetaddon/admin.py
+++ hetaddon/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Folder)
+admin.site.register(Document)
+admin.site.register(Requirement)
+admin.site.register(User)
+admin.site.register(Section)
+admin.site.register(DocumentCategory)
+admin.site.register(Membership)
+admin.site.register(ExternalPlatform)
